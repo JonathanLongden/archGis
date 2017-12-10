8dc427b74d017e5b85d77388e912529b956aee2e
WC\jlongden <longden.jonathanedgar@gmail.com>
2017-12-09 22:19:39 -0700

    update header

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, MenuItem, Image,Row, Col } from 'react-bootstrap';
+import Logo from '../img/logo.PNG';
 //import { LinkContainer } from 'react-router-bootstrap';
 //import { Link } from 'react-router-dom';
 
@@ -11,29 +12,41 @@ class Header extends Component {
       //user: props.SuperUser.user
     }
   }
-
+//   <div style={{ width: 660, height: 'auto' }}>
+//   <ResponsiveEmbed a16by9>
+//     <embed type="image/svg+xml" src="/assets/TheresaKnott_castle.svg" />
+//   </ResponsiveEmbed>
+// </div>
+// <Image className="Logo" src={Logo} responsive/>
   render() {
-    return (<div>
-      <Navbar fluid  collapseOnSelect  >
-        <Navbar.Header className="pull-left nopadding">
+    return (
+      <div className="container-fluid">
+        <Navbar fluid  collapseOnSelect  >
+          <Navbar.Header className="pull-left nopadding">
+            <Navbar.Brand>
+              <div style={{ width: 100, height: 'auto' }}>
+                <Image src={Logo} responsive/>
+              </div>
+            </Navbar.Brand>
+          </Navbar.Header>
 
-        </Navbar.Header>
+          <Navbar.Collapse className="pull-left">
+            <Navbar.Text>
+              <h1>Geospaitial Database Viewer</h1>
+              <p>Natural Resources Department, Kalispel Tribe of Indians</p>
+            </Navbar.Text>
+          </Navbar.Collapse>
 
-        <Navbar.Collapse className="pull-left">
-          <Navbar.Text>
-              Welcome! {"  " + this.state.user}
-          </Navbar.Text>
-        </Navbar.Collapse>
-        <Nav className="pull-right" bsStyle="pills" >
 
-          <NavDropdown eventKey={3} title="My Work" id="basic-nav-dropdown">
-
-              <MenuItem eventKey={3.2}>Pokemon Api</MenuItem>
-              <MenuItem eventKey={3.3}>Future</MenuItem>
-          </NavDropdown>
-        </Nav>
-    </Navbar>
-      </div>);
+          <Nav className="pull-right" bsStyle="pills" >
+            <NavDropdown eventKey={3} title="My Work" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.2}>Pokemon Api</MenuItem>
+                <MenuItem eventKey={3.3}>Future</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
+      </div>
+  );
   }
 }
 
